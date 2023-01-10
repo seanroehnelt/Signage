daef9bfa6b6dbaf12071876d39a9ce9258eb1c87
Sean Roehnelt <sean.roehnelt@hulu.com>
2023-01-10 20:54:49 +0700

    Initial Commit

---
+++ SignageUITests/SignageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SignageUITestsLaunchTests.swift
+//  SignageUITests
+//
+//  Created by Sean Roehnelt on 1/10/23.
+//
+
+import XCTest
+
+class SignageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
